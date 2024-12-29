403c158e64c7320c5d187592abcd17b52a68bca1
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2024-12-29 13:14:17 +0900

    first commit

---
+++ target/debug/deps/comments-32d74db152a906cc.d
@@ -0,0 +1,5 @@
+C:\Users\airon\Desktop\rust\comments\target\debug\deps\libcomments-32d74db152a906cc.rmeta: src/main.rs
+
+C:\Users\airon\Desktop\rust\comments\target\debug\deps\comments-32d74db152a906cc.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/comments-786f59d314d5829f.d
@@ -0,0 +1,5 @@
+C:\Users\airon\Desktop\rust\comments\target\debug\deps\libcomments-786f59d314d5829f.rmeta: src/main.rs
+
+C:\Users\airon\Desktop\rust\comments\target\debug\deps\comments-786f59d314d5829f.d: src/main.rs
+
+src/main.rs:
